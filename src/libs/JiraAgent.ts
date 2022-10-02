f2f0d16879ca7ec1d8ede90febb1ac1d642fb3c4
kost0806 <kimmin0806@gmail.com>
2022-10-02 19:23:09 +0900

    🚧 JiraAgent는 사내에서 완성 필요

--- src/libs/JiraAgent.ts
+++ src/libs/JiraAgent.ts
@@ -1,8 +1,20 @@
 import JiraIssue from '../types/JiraIssue';
+import JiraApi from 'jira-client';
 import NotImplementedError from '../errors/NotImplementedError';
+import IJiraAgent from '../interfaces/IJiraAgent';
 
-class JiraAgent {
-  constructor() {}
+class JiraAgent implements IJiraAgent {
+  jiraApi: any;
+  constructor(username: string, password: string) {
+    this.jiraApi = new JiraApi({
+      protocol: 'https',
+      host: 'https://networkrndhub.sec.samsung.net',
+      username: username,
+      password: password,
+      apiVersion: '2',
+      strictSSL: true,
+    });
+  }
 
   createIssue(summary: string, description: string): JiraIssue {
     throw new NotImplementedError();
--- test/libs/JiraAgent.spec.ts
+++ test/libs/JiraAgent.spec.ts
@@ -3,11 +3,11 @@ import JiraAgent from '../../src/libs/JiraAgent';
 
 describe('GitHubAgent Test', () => {
   it('creating test', () => {
-    expect(() => new JiraAgent()).not.toThrow();
+    expect(() => new JiraAgent('', '')).not.toThrow();
   });
 
   it('creating issue', async () => {
-    const agent = new JiraAgent();
+    const agent = new JiraAgent('', '');
 
     expect(() =>
       agent.createIssue('GIVEN SUMMARY', 'GIVEN DESCRIPTION')
